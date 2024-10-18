7333a55855740657fa837ee575e7d1a08772295e
Nana Tsitsikashvili <ntsitsikashvili@credo.ge>
2024-10-18 14:00:14 +0400

    task3

---
+++ src/test/java/task3.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class task3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("ჩაწერეთ ციფრი: ");
+        int number = scanner.nextInt();
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(number + " x " + i + " = " + (number * i));
+        }
+
+        scanner.close();
+    }
+}
